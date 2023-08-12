339532062f1f704ab8639219f4492c4019117c1b
kein12345 <141533336+kein12345@users.noreply.github.com>
2023-08-12 10:49:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-import time
+import tkinter as tk
+from tkinter import *
 import serial.tools.list_ports
+# window = tk.Tk()
+# window.title("iot project")
+# window.attributes('-fullscreen', False)
+# screen_width = window.winfo_screenwidth()
+# screen_height = window.winfo_screenheight()
+# print("Size:", screen_width, screen_height)
 
 try:
-    ser = serial.Serial(port = "/dev/ttyAMA2", baudrate = 9600)
-    print("Open COM successfully")
+    ser = serial.Serial(port="COM5",baudrate=9600)
+    print("success")
 except:
-    print('Can not open the port')
+    print("cant not open port")
 
 relay1_ON = [1,6,0,0,0,255,200,91]
 relay1_OFF = [1,6,0,0,0,0,137,202]
@@ -25,51 +32,84 @@ relay5_OFF = [5,6,0,0,0,0,136,78]
 relay6_ON = [6,6,0,0,0,255,200,61]
 relay6_OFF = [6,6,0,0,0,0,136,125]
 
-def set_device1(state):
+
+def setdevice1(state):
     if state == True:
         ser.write(relay1_ON)
     else:
         ser.write(relay1_OFF)
-    # time.sleep(1)
-    # return serial_read_data(ser)
 
-def set_device2(state):
+
+def setdevice2(state):
     if state == True:
         ser.write(relay2_ON)
     else:
         ser.write(relay2_OFF)
 
-def serial_read_data(ser):
-    bytesToRead = ser.inWaiting()
-    if bytesToRead > 0:
-        out = ser.read(bytesToRead)
-        data_array = [b for b in out]
-        print(data_array)
-        if len(data_array) >= 7:
-            array_size = len(data_array)
-            value = data_array[array_size - 4] * 256 + data_array[array_size - 3]
-            return value
-        else:
-            return -1
-    return 0
 
-soil_temperature =[1, 3, 0, 6, 0, 1, 100, 11]
-def readTemperature():
-    serial_read_data(ser)
-    ser.write(soil_temperature)
-    time.sleep(1)
-    return serial_read_data(ser)
-
-soil_moisture = [1, 3, 0, 7, 0, 1, 53, 203]
-def readMoisture():
-    serial_read_data(ser)
-    ser.write(soil_moisture)
-    time.sleep(1)
-    return serial_read_data(ser)
+def setdevice3(state):
+    if state == True:
+        ser.write(relay1_ON)
+    else:
+        ser.write(relay1_OFF)
 
 
-print("Relay 1 ON")
-ser.write(relay1_ON)
-time.sleep(2)
-print("Relay 1 OFF")
-ser.write(relay1_OFF)
+def setdevice4(state):
+    if state == True:
+        ser.write(relay2_ON)
+    else:
+        ser.write(relay2_OFF)
+
+
+def setdevice5(state):
+    if state == True:
+        ser.write(relay1_ON)
+    else:
+        ser.write(relay1_OFF)
+
+
+def setdevice6(state):
+    if state == True:
+        ser.write(relay2_ON)
+    else:
+        ser.write(relay2_OFF)
+
+
+while True:
+    print("Relay 1 ON")
+    ser.write(relay1_ON)
+    time.sleep(2)
+    print("Relay 1 OFF")
+    ser.write(relay1_OFF)
+    
+    print("Relay 2 ON")
+    ser.write(relay1_ON)
+    time.sleep(2)
+    print("Relay 2 OFF")
+    ser.write(relay1_OFF)
+    
+    print("Relay 3 ON")
+    ser.write(relay1_ON)
+    time.sleep(2)
+    print("Relay 3 OFF")
+    ser.write(relay1_OFF)
+    
+    print("Relay 4 ON")
+    ser.write(relay1_ON)
+    time.sleep(2)
+    print("Relay 4 OFF")
+    ser.write(relay1_OFF)
+    
+    print("Relay 5 ON")
+    ser.write(relay1_ON)
+    time.sleep(2)
+    print("Relay 5 OFF")
+    ser.write(relay1_OFF)
+    
+    print("Relay 6 ON")
+    ser.write(relay1_ON)
+    time.sleep(2)
+    print("Relay 6 OFF")
+    ser.write(relay1_OFF)
+
+
